41da49341d08e2b5f960833056cd1bb6c93e56c3
kbmackenzie <kelly.a.betty@gmail.com>
2025-12-15 04:12:27 -0300

    refactor: Do a little clean-up on CLI interface

--- logocrawler/__main__.py
+++ logocrawler/__main__.py
@@ -2,8 +2,7 @@ import csv
 import sys
 from .crawler import find_logo, WebsiteInfo
 
-# A simple CLI interface.
-# In the real world, I would use proper argument parsing.
+# A simple CLI interface. In the real world, I would use proper argument parsing.
 # As this is just a test assignment, though, just indexing into sys.argv will do.
 
 # It accepts any filepath.
@@ -25,14 +24,17 @@ def write_logo_data(websites: list[WebsiteInfo]):
     """Write each website's logo and favicon to stdout as CSV rows."""
     writer = csv.writer(sys.stdout)
     for website in websites:
-        writer.writerow([website.url, website.logo or '', website.favicon or ''])
+        writer.writerow([
+            website.url,
+            website.logo or '',
+            website.favicon or ''
+        ])
 
-if sys.__name__ == '__main__':
-    input_file   = sys.argv[1] if len(sys.argv) > 1 else './websites.csv'
-    website_list = read_websites(input_file)
+input_file   = sys.argv[1] if len(sys.argv) > 1 else './websites.csv'
+website_list = read_websites(input_file)
 
-    websites: list[WebsiteInfo] = []
-    for website in website_list:
-        info = find_logo(website)
-        websites.append(info)
-    write_logo_data(websites)
+websites: list[WebsiteInfo] = []
+for website in website_list:
+    info = find_logo(website)
+    websites.append(info)
+write_logo_data(websites)
