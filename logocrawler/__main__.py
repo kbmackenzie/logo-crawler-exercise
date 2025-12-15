cb0273001327b52f1ce20452cd8d72fbabc9d335
kbmackenzie <kelly.a.betty@gmail.com>
2025-12-15 07:51:32 -0300

    Improve CLI interface

--- logocrawler/__main__.py
+++ logocrawler/__main__.py
@@ -1,12 +1,12 @@
 import csv
 import sys
-from .crawler import find_logo, WebsiteInfo
+from .crawler import find_logo
 
 # A simple CLI interface. In the real world, I would use proper argument parsing.
 # As this is just a test assignment, though, just indexing into sys.argv will do.
 
 # It accepts any filepath.
-# If none is given, it defaults to reading a './website.csv' in the current directory.
+# If none is given, it defaults to reading a './websites.csv' in the current directory.
 
 def read_websites(csv_filepath: str) -> list[str]:
     """Read list of websites from a CSV file."""
@@ -20,21 +20,18 @@ def read_websites(csv_filepath: str) -> list[str]:
             websites.append(row[0])
     return websites
 
-def write_logo_data(websites: list[WebsiteInfo]):
+def write_logo_data(websites: list[str]):
     """Write each website's logo and favicon to stdout as CSV rows."""
     writer = csv.writer(sys.stdout)
     for website in websites:
+        info = find_logo(website)
         writer.writerow([
-            website.url,
-            website.logo or '',
-            website.favicon or ''
+            info.url,
+            info.logo or '',
+            info.favicon or ''
         ])
 
-input_file   = sys.argv[1] if len(sys.argv) > 1 else './websites.csv'
-website_list = read_websites(input_file)
-
-websites: list[WebsiteInfo] = []
-for website in website_list:
-    info = find_logo(website)
-    websites.append(info)
-write_logo_data(websites)
+if __name__ == "__main__":
+    input_file = sys.argv[1] if len(sys.argv) > 1 else './websites.csv'
+    websites   = read_websites(input_file)
+    write_logo_data(websites)
